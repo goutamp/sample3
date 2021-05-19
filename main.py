22f0302086d3efbb653cbad6604a9f8a98011aa5
Goutam Pramanick <pramanick.goutam@gmail.com>
2021-05-19 23:34:05 +0530

    fix!: this is major release

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def hello():
 
     """Return a friendly HTTP hello greeting."""
 
-    return 'Hello Istiosha_added_feature_branch_change_the_master_branch'
+    return 'Hello Istiosha_added_feature_branch'
 
 
 @app.errorhandler(500)
