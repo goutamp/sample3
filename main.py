7620d8763434f71959507c4d950273ed607e185f
Goutam Pramanick <pramanick.goutam@gmail.com>
2021-07-29 17:41:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def hello():
 
     """Return a friendly HTTP hello- greeting."""
 
-    return 'Hello Istiosh added fix update'
+    return 'Hello Istiosh added fix update qa push'
 
 
 @app.errorhandler(500)
